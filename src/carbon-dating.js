fd70087cc1993b8c343d40a14019269aee9a996a
AlenaChupina <chupina.elen@gmail.com>
2021-03-08 20:43:03 -0600

    feat:add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+ if( !sampleActivity || typeof sampleActivity !== "string" ) return false; 
+  let currentActivity = parseFloat(sampleActivity);
+  if( currentActivity <= 0 || currentActivity > MODERN_ACTIVITY ) return false;
+  if ( !isFinite(currentActivity)) return false;
+  
+
+  let time = Math.ceil(Math.log(MODERN_ACTIVITY /currentActivity )/(0.693/HALF_LIFE_PERIOD))
+  return time
+  
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-    
-}
+  if ( str === undefined) return "";
+  let addStr = options.addition;
+  let addRep = options.additionRepeatTimes;
+  let addSep = options.additionSeparator;
+
+  let strRep = options.repeatTimes;
+  let sep = options.separator;
+
+  let makeAddition = function (str, repeat, separator = "|") {
+      if( str === undefined)return "";   
+      let string = str;
+       
+    if (repeat) {
+      for (let i = 1; i < repeat; i++) {
+        string += separator + str;
+       
+      }
+     
+    } 
+    return string;
+  };
+ 
+  let partAddition = makeAddition(addStr, addRep, addSep);
+  let result = function(str1, str2 = "", repeat, separator = '+'){
+        let strConcat = str1 + str2;
+        let string = strConcat ;
+        if(repeat){
+        for(i=1;i<repeat;i++){
+        string += separator + strConcat;
+        }
+        
+        }
+        return string
+  }
+
+  return result(str, partAddition, strRep, sep);
+};
